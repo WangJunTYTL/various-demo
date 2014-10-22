684cefa623ed307cf4f9e8cd01c434577cf08917
WangJun <wangjuntytl@163.com>
2014-10-22 18:55:07 +0800

    加入redisdemo

---
+++ redisdemo/src/main/java/com/peaceful/activemq/TestQueueListener.java
@@ -0,0 +1,37 @@
+package com.peaceful.activemq;
+
+import com.peaceful.util.Util;
+import org.apache.activemq.network.jms.OutboundQueueBridge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Date 14/10/18.
+ * Author WangJun
+ * Email wangjuntytl@163.com
+ * <p/>
+ * 监听消息队列:testQueue
+ */
+public class TestQueueListener implements MessageListener {
+
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            String msg = textMessage.getText();
+            Util.report(msg);
+        } catch (JMSException e) {
+            logger.error("onMessage:{}", e);
+        }
+
+
+    }
+}
