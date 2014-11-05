37ba7d3f1c327120fefc66f9fe44d97be337fc12
WangJun <wangjuntytl@163.com>
2014-11-05 18:38:08 +0800

    更新脚本目录

--- script/jetty-auto-deploy-script/env.sh
+++ script/jetty-auto-deploy-script/env.sh
@@ -2,7 +2,7 @@
 #==========================================
 #         requerid params
 #====================================
-ROOT_DIR="/Users/wangjun/ideaWorkSpace/myapp/script/autoDeployScript/jetty-auto-deploy-demo/target"
+ROOT_DIR="/Users/wangjun/ideaWorkSpace/myapp/script/jetty-auto-deploy-demo/target"
 JETTY_HOME_DIR="/Users/wangjun/software/server/jetty-distribution-9.2.2.v20140723"
 CONF_HOME="${ROOT_DIR}/conf"
 LOG_HOME="${ROOT_DIR}/log"
