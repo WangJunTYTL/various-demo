8e2d14a02027c793a4dd30fde25e2cfc8f055350
WangJun <wangjuntytl@163.com>
2014-11-06 10:10:12 +0800

    codeUpdate

--- script/jetty-auto-deploy-script/env.sh
+++ script/jetty-auto-deploy-script/env.sh
@@ -2,13 +2,13 @@
 #==========================================
 #         requerid params
 #====================================
-ROOT_DIR="/Users/wangjun/ideaWorkSpace/myapp/script/jetty-auto-deploy-demo/target"
+TARGET_DIR="/Users/wangjun/ideaWorkSpace/myapp/script/jetty-auto-deploy-script/target"
 JETTY_HOME_DIR="/Users/wangjun/software/server/jetty-distribution-9.2.2.v20140723"
-CONF_HOME="${ROOT_DIR}/conf"
-LOG_HOME="${ROOT_DIR}/log"
-BUILD_HOME="${ROOT_DIR}/build"
-WEBAPP_HOME="${ROOT_DIR}/webapp"
-DEPLOY_HOME="${ROOT_DIR}/deploy"
-CONTROL_HOME="${ROOT_DIR}/control"
+CONF_HOME="${TARGTE_DIR}/conf"
+LOG_HOME="${TARGET_DIR}/log"
+BUILD_HOME="${TARGET_DIR}/build"
+WEBAPP_HOME="${TARGET_DIR}/webapp"
+DEPLOY_HOME="${TARGET_DIR}/deploy"
+CONTROL_HOME="${TARGET_DIR}/control"
 
 
