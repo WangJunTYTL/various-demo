45ffda0cda3d48a610885fcb77ef6a88647abdb8
WangJun <wangjuntytl@163.com>
2014-11-07 16:16:09 +0800

    python 异常处理

--- script/python-demo/pythonSyntax.py
+++ script/python-demo/pythonSyntax.py
@@ -8,7 +8,10 @@
 #desc python 基本语法学习
 #    
 #	repl:
-#	 	支持repl模式，如果不懂某个语法,可以使用help(object)
+#	 	支持repl模式，
+# 	
+# 	如果不懂某个语法,可以使用help(object)
+# 	查看模块所有的属性dir(object) [dir():查看当前]	
 #=================================
 import sys  #使用模块 sys模块包含了与Python解释器和它的环境有关的函数（可以在使用的位置导入，在头部导入不是必须的）
 import pythonModule #导入自定义的模块，模块导入类似与java的类导入
