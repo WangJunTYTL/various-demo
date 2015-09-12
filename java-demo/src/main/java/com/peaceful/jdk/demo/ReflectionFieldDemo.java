81bc4b4349ee0297f410a59d86b47b7219464c8e
wangjun <wangjun@edaijia-inc.cn>
2015-09-12 12:53:32 +0800

    codeUpdate

---
+++ java-demo/src/main/java/com/peaceful/jdk/demo/ReflectionFieldDemo.java
@@ -0,0 +1,42 @@
+package com.peaceful.jdk.demo;
+
+import com.peaceful.common.util.Util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author WangJun <wangjuntytl@163.com>
+ * @version 1.0 15/8/16
+ * @since 1.6
+ */
+
+public class ReflectionFieldDemo {
+
+    static class Hello {
+        public String name = "hello";
+        private int age = 28;
+        public static int gender = 1;
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+
+        //获取field 对象
+        Field nameField = Hello.class.getDeclaredField("name");
+        Util.report(nameField.get(new Hello()));
+        Field ageField = Hello.class.getDeclaredField("age");
+        ageField.setAccessible(true);
+        Util.report(ageField.get(new Hello()));
+        Field genderField = Hello.class.getDeclaredField("gender");
+        Util.report(genderField.get(new Hello()));
+
+
+//        Field[] fields = Hello.class.getFields(); // 不会返回私有变量
+        Field[] fields = Hello.class.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Util.report(field.getName() + ":" + field.get(new Hello()));
+        }
+    }
+
+}
