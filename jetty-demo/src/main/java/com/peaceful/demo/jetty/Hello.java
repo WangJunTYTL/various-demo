287adbe90072a5daed2cb4f9eaf4f9f71ae05194
王俊 <wangjun38@meituan.com>
2018-05-04 22:40:46 +0800

    update

--- jetty-demo/src/main/java/com/peaceful/demo/jetty/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.peaceful.demo.jetty;
-
-/**
- * Created by wangjun on 2016/11/12.
- */
-public interface Hello {
-
-    String say(String msg);
-}
--- jetty-demo/src/main/java/com/peaceful/demo/jetty/MyServlet.java
+++ jetty-demo/src/main/java/com/peaceful/demo/jetty/MyServlet.java
@@ -1,16 +1,15 @@
 package com.peaceful.demo.jetty;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by wangjun on 2016/11/13.
  */
-@WebServlet(urlPatterns = "/servlet3")
 public class MyServlet extends HttpServlet {
 
     @Override
